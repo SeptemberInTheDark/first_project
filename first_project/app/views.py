fd5c032330572bb2495c79484fb2340c55c65785
Андрей Соловьев <aa.solovev2@stalmail.org>
2026-03-06 16:39:10 +0300

    add routes

--- first_project/app/views.py
+++ first_project/app/views.py
@@ -1,3 +1,40 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+}
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+def index(request):
+    return render(request, 'index.html')
+
+def get_omlet(request):
+    context = {'recipe': DATA.get('omlet')}
+    return render(request, 'omlet.html', context)
+
+def get_pasta(request):
+    context = {'recipe': DATA.get('pasta')}
+    return render(request, 'pasta.html', context)
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app.views import index, get_omlet, get_pasta
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('omlet/', get_omlet),
+    path('pasta/', get_pasta),
 ]
